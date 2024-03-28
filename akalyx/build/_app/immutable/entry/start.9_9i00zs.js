6624db650e6c2909845d2a1f3899804b633ffb39
Gerrit R <vermistuk@gmail.com>
2024-03-28 17:01:46 +0100

    testing deployment

---
+++ akalyx/build/_app/immutable/entry/start.9_9i00zs.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DQJ8_ILD.js";export{t as start};
--- akalyx/svelte.config.js
+++ akalyx/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: '',
+			base: '/akalyx/build',
 			// base: '/akalyx',
 			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
