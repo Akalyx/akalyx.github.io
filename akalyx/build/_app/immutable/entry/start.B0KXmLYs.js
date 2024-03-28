9581f978a1a7c75f33bf7326158d3afa578e99d8
Gerrit R <vermistuk@gmail.com>
2024-03-28 16:22:03 +0100

    testing deployment

---
+++ akalyx/build/_app/immutable/entry/start.B0KXmLYs.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BuN7WN7y.js";export{t as start};
--- akalyx/svelte.config.js
+++ akalyx/svelte.config.js
@@ -3,6 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		prerender: true,
 		adapter: adapter({
 			fallback: '404.html'
 		}),
